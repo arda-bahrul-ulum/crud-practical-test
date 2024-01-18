ea11eba182c064463c7bd3b7c4ff34c3be52a3f1
arda-bahrul-ulum <arda.ulum27@gmail.com>
2024-01-18 15:25:38 +0700

    feat: add migration

---
+++ database/migrations/2024_01_18_082101_create_student_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->date('date_of_birth');
+            $table->string('gender');
+            $table->string('mobile_phone');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student');
+    }
+};
